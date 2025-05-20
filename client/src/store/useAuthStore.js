37c739457929cb435be9ecc824226b5ab72e200d
kush <kushal27001@gmail.com>
2025-05-20 13:34:11 +0530

    Profile update completed

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -62,6 +62,15 @@ export const useAuthStore = create((set) => ({
     },
 
     updateProfile: async (data)=>{
-        
+        set({isUpdatingProfile:true});
+        try {
+            const res = await axiosInstance.put("/auth/update-profile",data);
+            set({authUser:res.data});
+            toast.success("Profile Updated Successfully");
+        } catch (error) {
+            toast.error(error?.response?.data?.message)
+        } finally {
+            set({isUpdatingProfile:false});
+        }
     }
 }));
