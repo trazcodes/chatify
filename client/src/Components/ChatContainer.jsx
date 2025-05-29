9d1774b5010a2bb4130f3bf9ec0de1808218b3a6
kush <kushal27001@gmail.com>
2025-05-29 19:31:36 +0530

    Updated

--- client/src/Components/ChatContainer.jsx
+++ client/src/Components/ChatContainer.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import {useChatStore} from '../store/useChatStore'
+import ChatHeader from './ChatHeader';
+import MessageInput from './MessageInput';
 const ChatContainer = () => {
   const {messages, getMessages, isMessagesLoading, selectedUser}=useChatStore();
   useEffect(()=>{
