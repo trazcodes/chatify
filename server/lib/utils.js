3cef5971d665930f432fca8f108bfaa0b66664b3
kush <kushal27001@gmail.com>
2026-01-25 22:22:16 +0530

    Refactor generateToken function to improve cookie settings for cross-domain support

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,14 +1,20 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken=(userId,res)=>{
-    const token = jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:"7d"
-    })
-    res.cookie("jwt",token,{
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        httpOnly:true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development", // Set to true in production
-    })
-    return token;
-}
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+
+    // IMPORTANT for cross-domain
+    sameSite: "none",
+
+    // Must be true when sameSite = none
+    secure: true,
+  });
+
+  return token;
+};
